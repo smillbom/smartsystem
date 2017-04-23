52b2c49420c2e7e0d02b188728fe07621d3c9847
smillbom <chanon.sing@bumail.net>
2017-04-24 00:37:17 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,8 +17,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text
+				'type' => 'ไง',
+				'text' => 'ไงไต๋'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
