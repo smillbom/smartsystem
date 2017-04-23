72a2935b52659a84d1ec491deeb85d213f1b7e7e
smillbom <chanon.sing@bumail.net>
2017-04-24 02:47:43 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,13 +9,14 @@ $events = json_decode($content, true);
 
 if (!is_null($events['events'])) {
 	// Loop through each event
+	$num = 0;$name='';
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
 			$replyToken = $event['replyToken'];
-			$num = 0;$name='';
+			
 			if($text=='เปิดไฟ'){
 				
 			$messages = [
