aa0d20deeadbb53c7c3007d52343c1fe637f3c42
smillbom <chanon.sing@bumail.net>
2017-04-24 01:28:24 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -37,7 +37,7 @@ if (!is_null($events['events'])) {
 			}
 			
 			
-			/*
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -56,7 +56,7 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 
-			echo $result . "\r\n";*/
+			echo $result . "\r\n";
 		}
 	}
 }
