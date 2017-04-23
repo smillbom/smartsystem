78f5c629e1b3bc486c750e01eb6feb68d906fd1e
smillbom <chanon.sing@bumail.net>
2017-04-24 00:42:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,14 +12,28 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-			// Get replyToken
+			
+			if($text=='เปิดไฟ'){
+				// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'ไงไต๋'
+				'text' => 'เปิดไฟแล้ว'
 			];
+			}else{
+					// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+			}
+			
+			
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
