7565d5908957f4ecd5f1733f5bc0314e8eb02ecf
smillbom <chanon.sing@bumail.net>
2017-04-24 00:29:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,4 @@
-<?php
+<?php/*
 $access_token = 'hLkOg0z8Bb3Ch0auEZ/7m5/CoX1aFlASR2K4n7uzTDiPNSLASd1QKaAxyb1y8vUVLtghB9VRIfCqmCCn0sCawnCy8ItyNkIGWyEWT5GakNX3voD2oguz4jJWMkckbsLqbV3J1XratjLxjtWmNx0mRAdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
@@ -8,4 +8,5 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-echo $result;
+echo $result;*/
+echo 'bot';
